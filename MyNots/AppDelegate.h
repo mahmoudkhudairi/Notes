b60b548597aa0373a2705b88964d23ad0fe6ddb3
mahmoud khudairi <mahmoudkhudairi@gmail.com>
2017-03-22 14:09:28 +0800

    Initial Commit

---
+++ MyNots/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyNots
+//
+//  Created by mahmoud khudairi on 3/22/17.
+//  Copyright © 2017 mahmoud khudairi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyNots/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyNots
+//
+//  Created by mahmoud khudairi on 3/22/17.
+//  Copyright © 2017 mahmoud khudairi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
